a0ded2bad6899f9515ea2afc4a0775314f80bb10
Gabriel Rocha <gabreuvcr@gmail.com>
2023-07-04 21:47:16 -0300

    More changes in Token...

--- Monkey/Lexing/Lexer.cs
+++ Monkey/Lexing/Lexer.cs
@@ -64,7 +64,7 @@ public class Lexer
             '\0' => Token.Eof,
             _ => Token.Illegal(_ch),
         };
-
+        
         ReadChar();
         return token;
     }
--- Monkey/Lexing/Token.cs
+++ Monkey/Lexing/Token.cs
@@ -1,41 +1,61 @@
 namespace Monkey.Lexing;
 
-public readonly record struct Token
+public enum TokenType {
+    Equal,
+    NotEqual,
+    Assign,
+    Plus,
+    Minus,
+    Bang,
+    Asterisk,
+    Slash,
+    LessThan,
+    GreaterThan,
+    Comma,
+    Semicolon,
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Function,
+    Let,
+    If,
+    Else,
+    Return,
+    True,
+    False,
+    Eof,
+    Ident,
+    Int,
+    Illegal,
+}
+
+public record Token(TokenType Type, string Literal)
 {
-    public readonly TokenType Type;
-    public readonly string Literal;
-    
-    private Token(TokenType type, string literal)
-    {
-        Type = type;
-        Literal = literal;
-    }
-
-    public static readonly Token Equal = new(TokenType.Equal, "==");
-    public static readonly Token NotEqual = new(TokenType.NotEqual, "!=");
-    public static readonly Token Assign = new(TokenType.Assign, "=");
-    public static readonly Token Plus = new(TokenType.Plus, "+");
-    public static readonly Token Minus = new(TokenType.Minus, "-");
-    public static readonly Token Bang = new(TokenType.Bang, "!");
-    public static readonly Token Asterisk = new(TokenType.Asterisk, "*");
-    public static readonly Token Slash = new(TokenType.Slash, "/");
-    public static readonly Token LessThan = new(TokenType.LessThan, "<");
-    public static readonly Token GreaterThan = new(TokenType.GreaterThan, ">");
-    public static readonly Token Comma = new(TokenType.Comma, ",");
-    public static readonly Token Semicolon = new(TokenType.Semicolon, ";");
-    public static readonly Token LeftParen = new(TokenType.LeftParen, "(");
-    public static readonly Token RightParen = new(TokenType.RightParen, ")");
-    public static readonly Token LeftBrace = new(TokenType.LeftBrace, "{");
-    public static readonly Token RightBrace = new(TokenType.RightBrace, "}");
-    public static readonly Token Function = new(TokenType.Function, "fn");
-    public static readonly Token Let = new(TokenType.Let, "let");
-    public static readonly Token If = new(TokenType.If, "if");
-    public static readonly Token Else = new(TokenType.Else, "else");
-    public static readonly Token Return = new(TokenType.Return, "return");
-    public static readonly Token True = new(TokenType.True, "true");
-    public static readonly Token False = new(TokenType.False, "false");
-    public static readonly Token Eof = new(TokenType.Eof, string.Empty);
-
+    public static Token Equal => new(TokenType.Equal, "==");
+    public static Token NotEqual => new(TokenType.NotEqual, "!=");
+    public static Token Assign => new(TokenType.Assign, "=");
+    public static Token Plus => new(TokenType.Plus, "+");
+    public static Token Minus => new(TokenType.Minus, "-");
+    public static Token Bang => new(TokenType.Bang, "!");
+    public static Token Asterisk => new(TokenType.Asterisk, "*");
+    public static Token Slash => new(TokenType.Slash, "/");
+    public static Token LessThan => new(TokenType.LessThan, "<");
+    public static Token GreaterThan => new(TokenType.GreaterThan, ">");
+    public static Token Comma => new(TokenType.Comma, ",");
+    public static Token Semicolon => new(TokenType.Semicolon, ";");
+    public static Token LeftParen => new(TokenType.LeftParen, "(");
+    public static Token RightParen => new(TokenType.RightParen, ")");
+    public static Token LeftBrace => new(TokenType.LeftBrace, "{");
+    public static Token RightBrace => new(TokenType.RightBrace, "}");
+    public static Token Function => new(TokenType.Function, "fn");
+    public static Token Let => new(TokenType.Let, "let");
+    public static Token If => new(TokenType.If, "if");
+    public static Token Else => new(TokenType.Else, "else");
+    public static Token Return => new(TokenType.Return, "return");
+    public static Token True => new(TokenType.True, "true");
+    public static Token False => new(TokenType.False, "false");
+    public static Token Eof => new(TokenType.Eof, string.Empty);
     public static Token Ident(string literal) => new(TokenType.Ident, literal);
     public static Token Int(string literal) => new(TokenType.Int, literal);
     public static Token Illegal(char ch) => new(TokenType.Illegal, ch.ToString());
